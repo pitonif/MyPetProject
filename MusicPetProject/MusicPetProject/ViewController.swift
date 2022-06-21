8e7f4364a582588aa44f67bbeaf10dc35df467c6
Lobachkov Oleg <olobachkov@gmail.com>
2022-06-21 17:41:04 +0300

    create project

---
+++ MusicPetProject/MusicPetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPetProject
+//
+//  Created by Oleg on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
